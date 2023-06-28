5a8d1eeb4f837d4f12c574b2295bcca03f9b75c4
alex <abravov@wgu.edu>
2023-06-28 12:54:03 -0700

    About me

---
+++ Week_1/script.js
@@ -0,0 +1,5 @@
+let firstName = "Alejandro";
+let lastName = "Bravo-Vasquez";
+let yearsOfStudy = "2 quarters";
+let goal =
+  "Gain enough experience to launch my own web design and application business";
